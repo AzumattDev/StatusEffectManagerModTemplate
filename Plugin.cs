2e31a537dc5d0ec11f82f27cd61280206c393d7a
AzumattDev <azumattgaming@gmail.com>
2022-05-19 00:34:51 -0400

    Init

---
+++ Plugin.cs
@@ -0,0 +1,145 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using StatusEffectManager;
+using ServerSync;
+
+namespace StatusEffectManagerModTemplate
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class StatusEffectManagerModTemplatePlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "StatusEffectManagerModTemplate";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "{azumatt}";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource StatusEffectManagerModTemplateLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("General", "Force Server Config", true, "Force Server Config");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            CustomSE mycooleffect = new("Toxicity");
+            mycooleffect.Name.English("Toxicity");
+            mycooleffect.Type = EffectType.Consume;
+            mycooleffect.IconSprite = null;
+            mycooleffect.Name.German("Toxizität"); 
+            mycooleffect.Effect.m_startMessageType = MessageHud.MessageType.TopLeft;
+            mycooleffect.Effect.m_startMessage = "My Cool Status Effect Started"; 
+            mycooleffect.Effect.m_stopMessageType = MessageHud.MessageType.TopLeft;
+            mycooleffect.Effect.m_stopMessage = "Not cool anymore, ending effect."; 
+            mycooleffect.Effect.m_tooltip = "<color=orange>Toxic damage over time</color>"; 
+            mycooleffect.AddSEToPrefab(mycooleffect, "SwordIron");
+            
+            CustomSE drunkeffect = new("se_drunk", "se_drunk_effect");
+			drunkeffect.Name.English("Drunk"); // You can use this to fix the display name in code
+			drunkeffect.Icon = "DrunkIcon.png"; // Use this to add an icon (64x64) for the status effect. Put your icon in an "icons" folder
+			drunkeffect.Name.German("Betrunken"); // Or add translations for other languages
+			drunkeffect.Effect.m_startMessageType = MessageHud.MessageType.Center; // Specify where the start effect message shows
+			drunkeffect.Effect.m_startMessage = "I'm drunk!"; // What the start message says
+			drunkeffect.Effect.m_stopMessageType = MessageHud.MessageType.Center; // Specify where the stop effect message shows
+			drunkeffect.Effect.m_stopMessage = "Sober...again."; // What the stop message says
+			drunkeffect.Effect.m_tooltip = "<color=red>Your vision is blurry</color>"; // Tooltip that will describe the effect applied to the player
+			drunkeffect.AddSEToPrefab(drunkeffect, "TankardAnniversary"); // Adds the status effect to the Anniversary Tankard. Applies when equipped.
+			
+			// Create a new status effect in code and apply it to a prefab.
+			CustomSE codeSE = new("CodeStatusEffect");
+			codeSE.Name.English("New Effect");
+			codeSE.Type = EffectType.Consume; // Set the type of status effect this should be.
+			codeSE.Icon = "ModDevPower.png";
+			codeSE.Name.German("Betrunken"); // Or add translations for other languages
+			codeSE.Effect.m_startMessageType = MessageHud.MessageType.Center; // Specify where the start effect message shows
+			codeSE.Effect.m_startMessage = "Mod Dev power, granted."; // What the start message says
+			codeSE.Effect.m_stopMessageType = MessageHud.MessageType.Center; // Specify where the stop effect message shows
+			codeSE.Effect.m_stopMessage = "Mod Dev power, removed."; // What the stop message says
+			codeSE.Effect.m_tooltip = "<color=green>You now have Mod Dev POWER!</color>"; // Tooltip that will describe the effect applied to the player
+			codeSE.AddSEToPrefab(codeSE, "SwordCheat"); // Adds the status effect to the Cheat Sword. Applies when equipped.
+		
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                StatusEffectManagerModTemplateLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                StatusEffectManagerModTemplateLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                StatusEffectManagerModTemplateLogger.LogError(
+                    "Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<bool>? _serverConfigLocked;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        #endregion
+    }
+}
---
+++ VersionHandshake.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace StatusEffectManagerModTemplate
+{
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.OnNewConnection))]
+    public static class RegisterAndCheckVersion
+    {
+        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
+        {
+            // Register version check call
+            StatusEffectManagerModTemplatePlugin.StatusEffectManagerModTemplateLogger.LogDebug(
+                "Registering version RPC handler");
+            peer.m_rpc.Register($"{StatusEffectManagerModTemplatePlugin.ModName}_VersionCheck",
+                new Action<ZRpc, ZPackage>(RpcHandlers.RPC_StatusEffectManagerModTemplate_Version));
+
+            // Make calls to check versions
+            StatusEffectManagerModTemplatePlugin.StatusEffectManagerModTemplateLogger.LogInfo("Invoking version check");
+            ZPackage zpackage = new();
+            zpackage.Write(StatusEffectManagerModTemplatePlugin.ModVersion);
+            peer.m_rpc.Invoke($"{StatusEffectManagerModTemplatePlugin.ModName}_VersionCheck", zpackage);
+        }
+    }
+
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_PeerInfo))]
+    public static class VerifyClient
+    {
+        private static bool Prefix(ZRpc rpc, ZPackage pkg, ref ZNet __instance)
+        {
+            if (!__instance.IsServer() || RpcHandlers.ValidatedPeers.Contains(rpc)) return true;
+            // Disconnect peer if they didn't send mod version at all
+            StatusEffectManagerModTemplatePlugin.StatusEffectManagerModTemplateLogger.LogWarning(
+                $"Peer ({rpc.m_socket.GetHostName()}) never sent version or couldn't due to previous disconnect, disconnecting");
+            rpc.Invoke("Error", 3);
+            return false; // Prevent calling underlying method
+        }
+
+        private static void Postfix(ZNet __instance)
+        {
+            ZRoutedRpc.instance.InvokeRoutedRPC(ZRoutedRpc.instance.GetServerPeerID(), "RequestAdminSync",
+                new ZPackage());
+        }
+    }
+
+    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.ShowConnectError))]
+    public class ShowConnectionError
+    {
+        private static void Postfix(FejdStartup __instance)
+        {
+            if (__instance.m_connectionFailedPanel.activeSelf)
+                __instance.m_connectionFailedError.text += "\n" + StatusEffectManagerModTemplatePlugin.ConnectionError;
+        }
+    }
+
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.Disconnect))]
+    public static class RemoveDisconnectedPeerFromVerified
+    {
+        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
+        {
+            if (!__instance.IsServer()) return;
+            // Remove peer from validated list
+            StatusEffectManagerModTemplatePlugin.StatusEffectManagerModTemplateLogger.LogInfo(
+                $"Peer ({peer.m_rpc.m_socket.GetHostName()}) disconnected, removing from validated list");
+            _ = RpcHandlers.ValidatedPeers.Remove(peer.m_rpc);
+        }
+    }
+
+    public static class RpcHandlers
+    {
+        public static readonly List<ZRpc> ValidatedPeers = new();
+
+        public static void RPC_StatusEffectManagerModTemplate_Version(ZRpc rpc, ZPackage pkg)
+        {
+            string? version = pkg.ReadString();
+            StatusEffectManagerModTemplatePlugin.StatusEffectManagerModTemplateLogger.LogInfo("Version check, local: " +
+                StatusEffectManagerModTemplatePlugin.ModVersion +
+                ",  remote: " + version);
+            if (version != StatusEffectManagerModTemplatePlugin.ModVersion)
+            {
+                StatusEffectManagerModTemplatePlugin.ConnectionError =
+                    $"{StatusEffectManagerModTemplatePlugin.ModName} Installed: {StatusEffectManagerModTemplatePlugin.ModVersion}\n Needed: {version}";
+                if (!ZNet.instance.IsServer()) return;
+                // Different versions - force disconnect client from server
+                StatusEffectManagerModTemplatePlugin.StatusEffectManagerModTemplateLogger.LogWarning(
+                    $"Peer ({rpc.m_socket.GetHostName()}) has incompatible version, disconnecting");
+                rpc.Invoke("Error", 3);
+            }
+            else
+            {
+                if (!ZNet.instance.IsServer())
+                {
+                    // Enable mod on client if versions match
+                    StatusEffectManagerModTemplatePlugin.StatusEffectManagerModTemplateLogger.LogInfo(
+                        "Received same version from server!");
+                }
+                else
+                {
+                    // Add client to validated list
+                    StatusEffectManagerModTemplatePlugin.StatusEffectManagerModTemplateLogger.LogInfo(
+                        $"Adding peer ({rpc.m_socket.GetHostName()}) to validated list");
+                    ValidatedPeers.Add(rpc);
+                }
+            }
+        }
+    }
+}
