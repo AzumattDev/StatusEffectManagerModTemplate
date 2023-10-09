8117b208783caa960d5bc6e466a7d0c64e79c86a
AzumattDev <azumattgaming@gmail.com>
2023-10-09 08:53:29 -0400

    Update for Valheim 0.217.24

--- VersionHandshake.cs
+++ VersionHandshake.cs
@@ -55,8 +55,8 @@ namespace StatusEffectManagerModTemplate
         {
             if (__instance.m_connectionFailedPanel.activeSelf)
             {
-                __instance.m_connectionFailedError.resizeTextMaxSize = 25;
-                __instance.m_connectionFailedError.resizeTextMinSize = 15;
+                __instance.m_connectionFailedError.fontSizeMax = 25;
+                __instance.m_connectionFailedError.fontSizeMin = 15;
                 __instance.m_connectionFailedError.text += "\n" + StatusEffectManagerModTemplatePlugin.ConnectionError;
             }
         }
